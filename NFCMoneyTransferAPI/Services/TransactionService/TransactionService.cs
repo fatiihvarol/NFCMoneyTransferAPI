a36ed8d15b353dfa8dafabe07e1e3e35f08eee10
fatiihvarol <fatihvrl38@gmail.com>
2024-06-10 00:44:28 +0300

    controller added

--- NFCMoneyTransferAPI/Services/TransactionService/TransactionService.cs
+++ NFCMoneyTransferAPI/Services/TransactionService/TransactionService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using NFCMoneyTransferAPI.DbContext;
 using NFCMoneyTransferAPI.DTOs;
 using NFCMoneyTransferAPI.Entity;
@@ -16,43 +17,56 @@ namespace NFCMoneyTransferAPI.Services.TransactionService
 
         public async Task<TransactionDto> TransferFundsAsync(int fromAccountId, int toAccountId, decimal amount)
         {
-            var fromAccount = await _context.Accounts.FirstOrDefaultAsync(a => a.AccountID == fromAccountId);
-            var toAccount = await _context.Accounts.FirstOrDefaultAsync(a => a.AccountID == toAccountId);
-
-            if (fromAccount == null || toAccount == null)
+            using (IDbContextTransaction transaction = await _context.Database.BeginTransactionAsync())
             {
-                throw new ArgumentException("Account not found.");
+                try
+                {
+                    var fromAccount = await _context.Accounts.FirstOrDefaultAsync(a => a.AccountID == fromAccountId);
+                    var toAccount = await _context.Accounts.FirstOrDefaultAsync(a => a.AccountID == toAccountId);
+
+                    if (fromAccount == null || toAccount == null)
+                    {
+                        throw new ArgumentException("Account not found.");
+                    }
+
+                    if (fromAccount.Balance < amount)
+                    {
+                        throw new InvalidOperationException("Insufficient funds.");
+                    }
+
+                    fromAccount.Balance -= amount;
+                    toAccount.Balance += amount;
+
+                    var transactionRecord = new Transaction
+                    {
+                        Amount = amount,
+                        Date = DateTime.UtcNow,
+                        FromAccountID = fromAccountId,
+                        FromAccount = fromAccount,
+                        ToAccountID = toAccountId,
+                        ToAccount = toAccount
+                    };
+
+                    _context.Transactions.Add(transactionRecord);
+                    await _context.SaveChangesAsync();
+
+                    await transaction.CommitAsync();
+
+                    return new TransactionDto
+                    {
+                        TransactionID = transactionRecord.TransactionID,
+                        Amount = transactionRecord.Amount,
+                        Date = transactionRecord.Date,
+                        FromAccountID = transactionRecord.FromAccountID,
+                        ToAccountID = transactionRecord.ToAccountID
+                    };
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    throw;
+                }
             }
-
-            if (fromAccount.Balance < amount)
-            {
-                throw new InvalidOperationException("Insufficient funds.");
-            }
-
-            fromAccount.Balance -= amount;
-            toAccount.Balance += amount;
-
-            var transaction = new Transaction
-            {
-                Amount = amount,
-                Date = DateTime.UtcNow,
-                FromAccountID = fromAccountId,
-                FromAccount = fromAccount,
-                ToAccountID = toAccountId,
-                ToAccount = toAccount
-            };
-
-            _context.Transactions.Add(transaction);
-            await _context.SaveChangesAsync();
-
-            return new TransactionDto
-            {
-                TransactionID = transaction.TransactionID,
-                Amount = transaction.Amount,
-                Date = transaction.Date,
-                FromAccountID = transaction.FromAccountID,
-                ToAccountID = transaction.ToAccountID
-            };
         }
     }
 }
